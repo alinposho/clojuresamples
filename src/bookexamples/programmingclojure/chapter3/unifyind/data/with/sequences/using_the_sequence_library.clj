0e4e2aba451a3a42f2d7474ec9c6eb8a101d0a06
Alin Posorovaschi <alin.posorovaschi@workday.com>
2014-02-03 11:56:46 +0000

    Finish the Clojure sequence library examples

--- src/bookexamples/programmingclojure/chapter3/unifyind/data/with/sequences/using_the_sequence_library.clj
+++ src/bookexamples/programmingclojure/chapter3/unifyind/data/with/sequences/using_the_sequence_library.clj
@@ -54,6 +54,44 @@
 (map #(format "<p>%s</p>" %) ["the" "quick" "brown" "fox"])
 
 
+(reduce + (range 1 11))
+
+(flatten [1 2 '(30 {:a 3, :b 4, :c #{:k :l}})]) ;; does not go recursively, instead it flattens only the first level
+
+;; This is how one defines the flatMap function that is so prevalent in Scala collections
+(defn flat-map [f x]
+  (flatten (map f x)))
+
+(flat-map #(range %) (range 5 10))
+
+(for [word ["the" "quick" "brown" "fox"]]
+  (format "<p>%s<p>" word))
+
+(take 10 (for [n whole-numbers :when (even? n)] n))
+
+(for [n whole-numbers :while (even? n)] n)
+
+(for [file "ABCDEFGH" rank (range 1 9)]
+  (format "%c%d" file rank))
+
+;; Flawed. For some reason it does not work and I do not, yet, have the skill to make it work
+(def primes(concat  [2 3 5 7] 
+   (lazy-seq   (let [primes-from     (fn primes-from [n [f & r]]       (if (some #(zero? (rem n %))         (take-while #(<= (* % %) n) primes))         (recur (+ n f) r)         (lazy-seq (cons n (primes-from (+ n f) r))))) 
+      wheel (cycle[2 4 2 4 6 2 6 4 2 4 6 6 2 6 4 2 
+                   6 4 6 8 4 2 4 2 4 8 6 4 6 2 4 6                   2 6 6 4 2 4 6 2 6 4 2 4 2 10 2 10])]            (primes-from 11 wheel)))))
+
+
+(take 10 (primes))
+(def ordinals-and-primes (map vector (iterate inc 1) primes))
+(take 10 (ordinals-and-primes))
+
+
+(def x (for [i (range 1 3)] (do (println i) i)))
+x
+(doall x)
+
+
+
 
 
 
