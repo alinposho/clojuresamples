f6503d0298f17ede3f5155900fdfc6e81b2f8b3c
Alin Posorovaschi <alinposho@yahoo.com>
2014-02-14 20:08:29 +0000

    Start implementing the gulp function

---
+++ src/bookexamples/programmingclojure/chapter6/protocols/datatypes/programming_to_abstractions.clj
@@ -0,0 +1,9 @@
+(ns bookexamples.programmingclojure.chapter6.protocols.datatypes.programming-to-abstractions
+  (:import (java.io FileInputStream InputStreamReader BufferedReader)))
+
+
+(defn gulp [src]
+  "Reads the contents of the src and returns the result as string"
+  (let [sb (StringBuilder.)]))
+
+
