f358760f40bca4a2f0daa81566c5bc1b60fb3e47
Alin Posorovaschi <alinposho@yahoo.com>
2014-02-15 18:45:40 +0000

    Implement gulp and expectorate in terms of the new make-8 functions

--- src/bookexamples/programmingclojure/chapter6/protocols/datatypes/programming_to_abstractions.clj
+++ src/bookexamples/programmingclojure/chapter6/protocols/datatypes/programming_to_abstractions.clj
@@ -31,3 +31,29 @@
 (gulp "resources/text-file.txt")
 
 
+(defn make-reader [src]
+  (-> src FileInputStream. InputStreamReader. BufferedReader.)) 
+
+(defn make-writer [dst]
+  (-> dst FileOutputStream. OutputStreamWriter. BufferedWriter.)) 
+
+;; Redefine gulp and expectorate in terms of our two new functions
+(defn gulp [src]
+  "Reads the contents of the src and returns the result as string"
+  (let [sb (StringBuilder.)]
+    (with-open [reader (make-reader src)]
+    (loop [c (.read reader)]
+      (if (neg? c)
+        (str sb)
+        (do 
+          (.append sb (char c))
+          (recur (.read reader))))))))
+
+
+(defn expectorate [dst content]
+  (with-open [writer (make-writer dst)]
+    (.write writer content)))
+
+(expectorate "resources/text-file.txt" "Some string to be put on the file using the new make-* funtions")
+(gulp "resources/text-file.txt")
+
