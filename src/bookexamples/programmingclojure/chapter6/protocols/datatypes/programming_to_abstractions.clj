caba865a252702922a8e6f5756ebd7fe1119a7c1
Alin Posorovaschi <alinposho@yahoo.com>
2014-02-15 18:41:06 +0000

    Implement gulp and expectorate and add test files as resources

--- src/bookexamples/programmingclojure/chapter6/protocols/datatypes/programming_to_abstractions.clj
+++ src/bookexamples/programmingclojure/chapter6/protocols/datatypes/programming_to_abstractions.clj
@@ -1,9 +1,33 @@
 (ns bookexamples.programmingclojure.chapter6.protocols.datatypes.programming-to-abstractions
-  (:import (java.io FileInputStream InputStreamReader BufferedReader)))
+  (:import (java.io FileInputStream InputStreamReader BufferedReader
+                    FileOutputStream OutputStreamWriter BufferedWriter)))
 
 
 (defn gulp [src]
   "Reads the contents of the src and returns the result as string"
-  (let [sb (StringBuilder.)]))
+  (let [sb (StringBuilder.)]
+    (with-open [reader (-> src 
+                           FileInputStream.
+                           InputStreamReader.
+                           BufferedReader.)]
+    (loop [c (.read reader)]
+      (if (neg? c)
+        (str sb)
+        (do 
+          (.append sb (char c))
+          (recur (.read reader))))))))
+
+
+(gulp "resources/some-file.txt")
+
+(defn expectorate [dst content]
+  (with-open [writer (-> dst 
+                         FileOutputStream.
+                         OutputStreamWriter.
+                         BufferedWriter.)]
+    (.write writer content)))
+
+(expectorate "resources/text-file.txt" "Some string to be put on the file")
+(gulp "resources/text-file.txt")
 
 
