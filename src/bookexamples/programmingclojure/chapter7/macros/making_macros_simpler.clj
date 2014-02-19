83f4e42a4ba8d5a2f48e19315807af937d915fa4
Alin Posorovaschi <alin.posorovaschi@workday.com>
2014-02-19 06:59:21 +0000

    Fix the chain multiple argument list implementation

--- src/bookexamples/programmingclojure/chapter7/macros/making_macros_simpler.clj
+++ src/bookexamples/programmingclojure/chapter7/macros/making_macros_simpler.clj
@@ -5,7 +5,8 @@
   (list '. x form))
 
 (def lst (java.util.ArrayList.))
-(chain list isEmpty)
+(chain lst isEmpty)
+(. lst add "element")
 
 ;; Extend chain to be equivalent to ..
 
@@ -15,11 +16,24 @@
 
 (defmacro chain 
   ([x form] 
-    (list '. x form))
-  ([x form & more]
-    (concat (list 'chain (list '. x form) more)) ))
+     (list '. x form))
+  ([x form & more] 
+    (concat 
+      (list 'chain (list '. x form)) 
+      more)))
 
 
 (macroexpand '(chain lst toArray length))
 
-(chain lst toArray length)
+;; This will fail to execute since "length" is a field on the array class
+;; whereas the dot operator expects a method
+;(chain lst toArray length)
+
+;; But this will work
+(chain lst iterator next)
+
+(macroexpand '(chain lst iterator next length))
+(chain lst iterator next length)
+
+
+
