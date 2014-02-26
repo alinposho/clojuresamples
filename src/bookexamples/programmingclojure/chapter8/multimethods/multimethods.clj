37572b4dd241b40f07eaf99530d4581f3d9926fc
Alin Posorovaschi <alin.posorovaschi@workday.com>
2014-02-26 06:58:59 +0000

    my-print prints vectors as well

--- src/bookexamples/programmingclojure/chapter8/multimethods/multimethods.clj
+++ src/bookexamples/programmingclojure/chapter8/multimethods/multimethods.clj
@@ -1,4 +1,5 @@
-(ns bookexamples.programmingclojure.chapter8.multimethods.multimethods)
+(ns bookexamples.programmingclojure.chapter8.multimethods.multimethods
+  [:require [clojure.string :as str]])
 
 ;; Living without muyltimethods
 (defn my-print [expr]
@@ -25,3 +26,16 @@
 ;; This will not raise an exception but it will not print the vector either
 (my-println [1 2 3])
 
+(defn my-print-vector [ob] 
+  (.write *out* "[")
+  (.write *out* (str/join ", " ob))
+  (.write *out* "]"))
+
+(defn my-print [ob] 
+  (cond
+    (nil? ob) (.write *out* "nil")
+    (string? ob) (.write *out* ob)
+    (vector? ob) (my-print-vector ob)))
+(my-println "Hello")
+(my-println nil)
+(my-println [1 2 3])
