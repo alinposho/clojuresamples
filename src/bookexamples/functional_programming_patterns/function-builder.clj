7d84629729374e82f85a3be3af929e3c26a556d2
Alin Posorovaschi <alinposho@yahoo.com>
2014-03-22 15:58:21 +0000

    An example of function composition in Clojure

--- src/bookexamples/functional_programming_patterns/function-builder.clj
+++ src/bookexamples/functional_programming_patterns/function-builder.clj
@@ -3,3 +3,11 @@
 (def person {:address {:street {:name "Fake Street"}}})
 
 (get-in person [:address :street :name])
+
+;; And now for some function composition
+
+(defn append-a [s] (str s "a"))
+(defn append-b [s] (str s "b"))
+(defn append-c [s] (str s "c"))
+
+(def append-cba (comp append-a append-b append-c))
