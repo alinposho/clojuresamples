e661ade49d2ba986ac5dba763633ed32fde51a13
Alin Posorovaschi <alin.posorovaschi@workday.com>
2014-05-17 17:19:09 +0100

    Add core.async as a dependency

--- project.clj
+++ project.clj
@@ -7,8 +7,9 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [log4j "1.2.16"]
-                 [javax.mail/mail "1.4.1"]]
-  :main ^:skip-aot bookexamples.programmingclojure.chapter9.real.world.example.pinger-main
+                 [javax.mail/mail "1.4.1"]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
+  ; :main ^:skip-aot bookexamples.programmingclojure.chapter9.real.world.example.pinger-main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
   :plugins [[lein-idefiles "0.2.0"]])
