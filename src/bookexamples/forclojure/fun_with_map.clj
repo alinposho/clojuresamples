b7b05cc4189bde29f5b0bde40733416f50371f79
Alin Posorovaschi <alin.posorovaschi@workday.com>
2014-06-19 10:17:45 +0100

    Interleave implementation

--- src/bookexamples/forclojure/fun_with_map.clj
+++ src/bookexamples/forclojure/fun_with_map.clj
@@ -1,3 +1,25 @@
 (ns bookexamples.forclojure.fun-with-map)
 
 (= '(6 7 8) (map #(+ % 5) '(1 2 3)))
+
+(def m {1 :a 2 :b 3 nil})
+(def k 6)
+
+(defn contains-null-value [k m]
+	(and (not (empty? (select-keys m [k]))) (nil? (m k))))
+
+(contains-null-value 3 m) ;; true
+(contains-null-value 4 m) ;; false
+
+
+
+;;; Interleave Two Seqs
+(defn my-interleave [xs ys] 
+	(letfn [(intrlv [acc [x & xs] [y & ys]]
+				(if (or (nil? x) (nil? y)) (reverse acc)
+					(recur (conj acc x y) xs ys)))]
+	(intrlv '() xs ys)))
+
+(my-interleave [1 2 3] '(:a :b))
+
+(mapcat #(list %1 %2) [1 2 3] '(:a :b))
