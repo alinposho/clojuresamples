01feed984b0bfc998116c715fdb2fe6705d2f31e
Alin Posorovaschi <alinposho@yahoo.com>
2014-06-21 18:17:00 +0100

    Add solutions from the web for comp implementation

--- src/bookexamples/forclojure/comp.clj
+++ src/bookexamples/forclojure/comp.clj
@@ -10,10 +10,14 @@
 (refer 'bookexamples.forclojure.comp)
 
 ((my-comp first rest reverse) [1 2 3 4])
-(my-comp [1 2 3 4 5])
+((my-comp zero? #(mod % 8) +) 3 5 7 9)
 
 ;; Some of the solutions on the web
-(#(map (vec %) (range (dec (count %)) -1 -1)) '(1 2 3 4))
-(#(into () %) [1 2 3 4])
+(((fn [& x] (reduce (fn [f g] (fn [& a] (f (apply g a)))) x)) zero? #(mod % 8) +) 3 5 7 9 )
+
+(fn comb [& funcs]
+  (fn [& args]
+    (first
+      (reduce #(vector (apply %2 %1)) args (reverse funcs )))))
 
 )
