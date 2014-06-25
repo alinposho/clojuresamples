805b3192c617385726bcdbecaae0ef29f6c5eda2
Alin Posorovaschi <alinposho@yahoo.com>
2014-06-25 20:57:06 +0100

    Add some solutions from the web to the power set problem

--- src/bookexamples/forclojure/power-set.clj
+++ src/bookexamples/forclojure/power-set.clj
@@ -7,16 +7,17 @@
 					(recur xs (set (for [x xs ys acc] (conj ys x))))))]
 	(gen-next xs #{#{}})))
 
+;; Intermediary function definition
+(defn gen-next [xs acc]
+	(if (contains? acc xs) 
+		(conj acc #{})
+		(recur xs (set (for [x xs ys acc] (conj ys x))))))
+
 
 (comment
 
 (load-file "src/bookexamples/forclojure/power-set.clj")
 (refer 'bookexamples.forclojure.power-set)
-
-(defn gen-next [xs acc]
-	(if (contains? acc xs) 
-		(conj acc #{})
-		(recur xs (set (for [x xs ys acc] (conj ys x))))))
 
 (gen-next #{1 2 3} #{#{}})
 
@@ -30,8 +31,20 @@
 
 
 ;; Some of the solutions on the web
-(not= true true false)
-(not= true true)
-(not= false false false)
+(fn power-set [coll]
+    (letfn [(union [s1 s2] (reduce conj s1 s2))]
+      (if (empty? coll)
+        #{#{}}
+        (let [element (first coll)
+              sub1 (power-set (disj coll element))
+              sub2 (set (map #(union #{element} %) sub1))]
+          (union sub1 sub2)))))
+
+(fn pow [s] (if (empty? s) #{s} (let [
+    s1 (first s)
+    sn (disj s s1)
+    pp (pow sn)
+    pn (for [i pp] (conj i s1))]
+    (into pp pn)))
 
 )
