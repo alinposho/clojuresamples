e36c13eb26580c18cacb4ca7c6f8414ad6daab3a
Alin Posorovaschi <alinposho@yahoo.com>
2014-08-30 19:35:19 +0100

    Add web solutions for the partial seq flattening problem

--- src/bookexamples/forclojure/partially-flatten.clj
+++ src/bookexamples/forclojure/partially-flatten.clj
@@ -19,26 +19,13 @@
 
 ;; Some of the solutions on the web
 ;; I don't want to know what this does
-(fn i                                                                                                                         
-  [& x]                                                                                                                       
-    (let [f first                                                                                                             
-          a apply                                                                                                     
-          [[b & e] & c] (sort-by f x)]                                                                                                         
-      (if (a = (map f x))                                                                                                             
-       b                                                                                                                   
-       (a i (conj c e)))))
+((fn _ [x]
+          (if 
+            (not (sequential? (first x))) [x]
+            (reduce concat (map _ x))
+            ))
+'((1 2)((3 4)((((5 6)))))))
 
-(fn lazy-pflatten [& colls]
-  (let [first-eles (map first colls)]
-    (if (every? #(= (first first-eles) %) first-eles)
-      (first first-eles)
-      (let [sorted-colls (sort-by first colls)]
-        (apply lazy-pflatten (cons (rest (first sorted-colls)) (rest sorted-colls)))))))
-
-(letfn [
-    (match? [x s] (or (= x (first s)) (and (> x (first s)) (recur x (rest s)))))
-    (first-common [s1 & sn] (first (drop-while #(not-every? (partial match? %) sn) s1)))]
-    first-common)
-
+(fn [x] (filter #(= % (flatten %)) (tree-seq sequential? seq x)))
 
 )
