bb7be4efe90585a7c454667dc63e77e79a0b6375
Alin Posorovaschi <alinposho@yahoo.com>
2014-09-14 14:16:44 +0100

    Code tidy up and add solutions from the web

--- src/bookexamples/forclojure/sequs-horribilis.clj
+++ src/bookexamples/forclojure/sequs-horribilis.clj
@@ -6,16 +6,11 @@
 	(letfn [(append-rst [pres sum [hd & tl]] 
 						(cond
 							(coll? hd) 
-							(let [elem (sequs (- n sum) (vec hd))]
-								(recur (conj pres elem) (apply + (flatten elem)) tl))
+								(let [elem (sequs (- n sum) (vec hd))]
+									(recur (conj pres elem) (apply + (flatten elem)) tl))
 							(or (nil? hd) (> (+ sum hd) n )) pres
 							:else (recur (conj pres hd) (+ sum hd) tl)))]
-		(if (not (nil? sq))
-			(append-rst [] 0 sq)
-				)))
-
-(sequs 10 [1 2 [3 [4 5] 6] 7])
-
+		(when-not (nil? sq)) (append-rst [] 0 sq)))
 
 (split-with integer? [1 2 [3 [4 5] 6] 7])
 
@@ -41,5 +36,54 @@
    '(-10 (1 (2 3 (4)))))
 
 ;; Some of the solutions on the web
+(fn __ 
+    ([limit col] (__ limit [] col))
+    ([limit agg [x & xs]]
+     (cond
+       (not x) agg
+       (sequential? x) 
+       (let [xsum (->> (flatten x) (reduce +))]
+         (if (<= xsum limit)
+           (__ (- limit xsum)
+               (conj agg x)
+               xs)
+           (conj agg
+                 (__ limit
+                     x))))
+       (> x limit) agg
+       :else (__ (- limit x) (conj agg x) xs)))
+    )
+
+(fn [max-sum coll]
+  (letfn [(step [coll current-sum max-sum]
+            (if (seq coll)
+              (let [head (first coll)]
+                (if (coll? head)
+                  (let [sub (step head current-sum max-sum)
+                        next-sum (+ current-sum (reduce + (flatten sub)))]
+                    (if (<= next-sum max-sum)
+                      (cons sub (step (next coll) next-sum max-sum))
+                      sub))
+                  (let [next-sum (+ head current-sum)]
+                    (if (<= next-sum max-sum)
+                      (cons head (step (next coll) next-sum max-sum))
+                      []))))))]
+    (step coll 0 max-sum)))
+
+(letfn [
+    (horribili [x t] (cond 
+        (number? t) (cond 
+            (>= x t) [(- x t) [t]] 
+            true     [nil []])
+        (empty? t)  [x [[]]]
+        true        (let 
+            [[xf tf] (horribili x (first t))]
+            (cond 
+                (nil? xf) [nil [tf]] 
+                true      (let 
+                    [[xn tn] (horribili xf (rest t))]
+                    [xn [(concat tf (first tn))]])))))]
+    (comp first second horribili))
+
 
 )
