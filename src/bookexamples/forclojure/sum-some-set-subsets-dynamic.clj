24aadeb764bb6e4cf7388ff9c3455a2771027d06
Alin Posorovaschi <alinposho@yahoo.com>
2014-09-19 22:45:10 +0100

    Dynamic programming solution to the Sum some subsets problem

--- src/bookexamples/forclojure/sum-some-set-subsets-dynamic.clj
+++ src/bookexamples/forclojure/sum-some-set-subsets-dynamic.clj
@@ -4,45 +4,45 @@
 ;; Given a variable number of sets of integers, create a function which returns true iff all of the sets have a non-empty subset with an equivalent summation.
 
 ;; Solution that does not work for large sets!!!
-(defn some-subsets? [& sts]
-  (letfn [(combinations [k st]
-            "Generates all combinations of subsets of st of length k and less"
-            (if (<= k 0) #{#{}}
-                (set 
-                  (for [s st
-                        subst (combinations (dec k) st)
-                        :let [nsubst (conj subst s)]]
-                    nsubst))))
-          (modified-combinations [st]
-            "Add the initial set to each subset creating pair of (subset set)"
-            (map #(list % st) (combinations (count st) st)))
-          (all-combinations-mapped [sts]
-            "All combinations of subsets grouped by subset sum"
-            (group-by #(apply + (first %)) (mapcat modified-combinations sts)))
-          (extract-initial-sets [sts]
-            (set (map (fn [vct] (set (map second (val vct)))) sts)))
-          (equiv-sum-sets [sts]
-            (filter #(= (set sts) %) (extract-initial-sets (all-combinations-mapped sts))))]
-    (not (empty? (equiv-sum-sets sts)))))
+(defn some-subsets? [& xs] 
+  (let [A (apply + (filter neg? (first xs)))
+        B (apply + (filter pos? (first xs)))
+        sum-subset? (fn [s x]
+                      (let [A (apply + (filter neg? x))
+                            B (apply + (filter pos? x))
+                            N (dec (count x))]
+                        (letfn [(Q [i s]
+                                  (cond 
+                                    (not (and (<= A s B) (<= 0 i N))) false
+                                    (zero? i) (= (x 0) s)
+                                    :else (or 
+                                            (= (x i) s)
+                                            (Q (dec i) s) 
+                                            (Q (dec i) (- s (x i))))))]
+                            ((memoize Q) N s))))]
+    (not-every? false? 
+      (map 
+        (fn [s] 
+          (every? true? 
+            (map #(sum-subset? s (vec %)) xs)))
+        (range A (inc B))))))
+
 
 (defn sum-subset? [s x]
   (let [A (apply + (filter neg? x))
         B (apply + (filter pos? x))
         N (dec (count x))]
     (letfn [(Q [i s]
-              (do 
-                (println (str "A=" A "B=" B "i=" i "s=" s))
               (cond 
                 (not (and (<= A s B) (<= 0 i N))) false
-                ; :else (do (println (str "A=" A "B=" B "i=" i "s=" s)) true)))]
-                (zero? s) (= (x 0) s)
-                :else 
-                      (or 
+                (zero? i) (= (x 0) s)
+                :else (or 
+                        (= (x i) s)
                         (Q (dec i) s) 
-                        (= (x i) s) 
-                        (Q (dec i) (- s (x i)))))))]
-      (let [mQ (memoize Q)]
-        (Q N s)))))
+                        (Q (dec i) (- s (x i))))))]
+        ((memoize Q) N s))))
+
+
 
 
 ;; Solution that does not work for large sets!!!
@@ -87,7 +87,29 @@
 )
 
 ;; Some of the solutions on the web
+(fn [& sets]
+  (not=
+    nil
+    (seq
+      (reduce
+        #(set (filter (fn [ele] (%1 ele)) %2))
+        (map
+          (fn [s]
+            (set
+              (map
+                #(reduce + %)
+                (filter
+                  #(seq %)
+                  (reduce #(concat %1 (map (fn [i] (set (conj i %2))) %1)) #{#{}} s)))))
+          sets)))))
 
+(fn common [& x] (letfn [
+    (subset-sums [s] (if (= 1 (count s)) (set s) (let [
+        s1 (first s)
+        ssn (subset-sums (next s))
+        ss1 (map (partial + s1) ssn)]
+        (set (concat ssn ss1 [s1])))))]
+    (boolean (seq (apply clojure.set/intersection (map subset-sums x))))))
 
 
 
