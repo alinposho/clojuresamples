e31cabaf921843df616de3ad9c46387c095fc1c0
Alin Posorovaschi <alinposho@yahoo.com>
2014-09-23 06:59:59 +0100

    Move to Clojure version 1.6.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"] 
+  :dependencies [[org.clojure/clojure "1.6.0"] 
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [log4j "1.2.16"]
