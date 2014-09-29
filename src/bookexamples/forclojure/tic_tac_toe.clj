2ac0a9b7a37b22d83d6fa6327cdd506a90edde21
Alin Posorovaschi <alinposho@yahoo.com>
2014-09-29 21:58:21 +0100

    Tic tac toe problem first working solution
     - Diagonal computation could be improved since it is imperative.

--- src/bookexamples/forclojure/tic_tac_toe.clj
+++ src/bookexamples/forclojure/tic_tac_toe.clj
@@ -2,9 +2,30 @@
 
 ;; Problem 73:
 ;;A tic-tac-toe board is represented by a two dimensional vector. X is represented by :x, O is represented by :o, and empty is represented by :e. A player wins by placing three Xs or three Os in a horizontal, vertical, or diagonal row. Write a function which analyzes a tic-tac-toe board and returns :x if X has won, :o if O has won, and nil if neither player has won.
-(defn analyze-ttt []
-  )
+(defn analyze-ttt [board]
+  (letfn [(columns [board]
+            (partition (count (first board)) (apply interleave board)))
+          (not-all-empty? [xs] (not-every? #{:e} xs))
+          (diag1 [board]
+            (loop [i 0 res []]
+              (if (>= i (count board))
+                res
+                (recur (inc i) (conj res ((board i) i))))))
+          (diag2 [board]
+            (loop [i 0 res []]
+              (if (>= i (count board))
+                res
+                (recur (inc i) (conj res ((board i) (- (count board) i 1)))))))]
+    (first (first (filter #(apply = %)
+     (filter not-all-empty?
+             (concat board (columns board) [(diag1 board) (diag2 board)])))))))
 
+(defn columns [board]
+  (partition (count (first board)) (apply interleave board)))
+
+(columns [[1 2 3]
+          [4 5 6]
+          [7 8 9]])
 
 
 (comment
--- test/bookexamples/forclojure/tic_tac_toe_test.clj
+++ test/bookexamples/forclojure/tic_tac_toe_test.clj
@@ -4,5 +4,25 @@
 
 (deftest tic-tac-toe-test
   (testing "Tests the tic tac toe board problem from 4Clojure"
-    ))
+    (is (= nil (analyze-ttt [[:e :e :e]
+                [:e :e :e]
+                [:e :e :e]])))
+    (is (= :x (analyze-ttt [[:x :e :o]
+               [:x :e :e]
+               [:x :e :o]])))
+    (is (= :o (analyze-ttt [[:e :x :e]
+               [:o :o :o]
+               [:x :e :x]])))
+    (is (= nil (analyze-ttt [[:x :e :o]
+                [:x :x :e]
+                [:o :x :o]])))
+    (is (= :x (analyze-ttt [[:x :e :e]
+               [:o :x :e]
+               [:o :e :x]])))
+    (is (= :o (analyze-ttt [[:x :e :o]
+               [:x :o :e]
+               [:o :e :x]])))
+    (is (= nil (analyze-ttt [[:x :o :x]
+                [:x :o :x]
+                [:o :x :o]])))))
 
