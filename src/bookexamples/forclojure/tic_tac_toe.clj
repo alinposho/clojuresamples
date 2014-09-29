33c071d59c9cb350679e998f06136dc579bcd50c
Alin Posorovaschi <alin.posorovaschi@workday.com>
2014-09-29 16:27:38 +0100

    Add the files for the tic tac toe board problem

---
+++ src/bookexamples/forclojure/tic_tac_toe.clj
@@ -0,0 +1,18 @@
+(ns bookexamples.forclojure.tic-tac-toe)
+
+;; Problem 73:
+;;A tic-tac-toe board is represented by a two dimensional vector. X is represented by :x, O is represented by :o, and empty is represented by :e. A player wins by placing three Xs or three Os in a horizontal, vertical, or diagonal row. Write a function which analyzes a tic-tac-toe board and returns :x if X has won, :o if O has won, and nil if neither player has won.
+(defn analyze-ttt []
+  )
+
+
+
+(comment
+
+(load-file "src/bookexamples/forclojure/tic_tac_toe.clj")
+(refer 'bookexamples.forclojure.analyze-ttt)
+
+
+;; Some of the solutions on the web
+
+)
---
+++ test/bookexamples/forclojure/tic_tac_toe_test.clj
@@ -0,0 +1,8 @@
+(ns bookexamples.forclojure.tic-tac-toe-test
+  (:require [clojure.test :refer :all]
+            [bookexamples.forclojure.tic-tac-toe :refer :all]))
+
+(deftest tic-tac-toe-test
+  (testing "Tests the tic tac toe board problem from 4Clojure"
+    ))
+
