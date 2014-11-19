f1e75818027190cec30663ff79f3a31c79dd94a3
Alin Posorovaschi <alinposho@yahoo.com>
2014-11-19 23:21:49 +0000

    Attempt to make Levenshtein distance problem solution work in 4clojure

--- src/bookexamples/forclojure/levenshtein_distance.clj
+++ src/bookexamples/forclojure/levenshtein_distance.clj
@@ -6,16 +6,28 @@
 ;;   - delete a single item
 ;;   - replace a single item with another item
 ;;  Function memoization should make the simple algorithm work like a dynamic programming algorith(see the memoization example)
-(def levenshtein
-  (memoize
-    (fn [xs ys]
-      (cond (zero? (count xs)) (count ys)
-            (zero? (count ys)) (count xs)
-            :else (min
-            (inc (levenshtein (rest xs) ys))
-            (inc (levenshtein xs (rest ys)))
-            (+   (levenshtein (rest xs) (rest ys))
-                 (if (= (first xs) (first ys)) 0 1)))))))
+;(def levenshtein
+;  (memoize
+;    (fn [xs ys]
+;      (cond (zero? (count xs)) (count ys)
+;            (zero? (count ys)) (count xs)
+;            :else (min
+;                    (inc (levenshtein (rest xs) ys))
+;                    (inc (levenshtein xs (rest ys)))
+;                    (+ (levenshtein (rest xs) (rest ys))
+;                       (if (= (first xs) (first ys)) 0 1)))))))
+
+(defn levenshtein [xs ys]
+  (let [lev (memoize
+              (fn [xs ys]
+                (cond (zero? (count xs)) (count ys)
+                      (zero? (count ys)) (count xs)
+                      :else (min
+                              (inc (lev (rest xs) ys))
+                              (inc (lev xs (rest ys)))
+                              (+ (lev (rest xs) (rest ys))
+                                 (if (= (first xs) (first ys)) 0 1))))))]
+    (lev xs ys)))
 
 
 (comment
@@ -23,16 +35,16 @@
   (load-file "src/bookexamples/forclojure/levenshtein_distance.clj")
   (refer 'bookexamples.forclojure.levenshtein-distance)
 
-  (= (levenshtein "kitten" "sitting") 3)
-  (= (levenshtein "closure" "clojure") (levenshtein "clojure" "closure") 1)
-  (= (levenshtein "xyx" "xyyyx") 2)
-  (= (levenshtein "" "123456") 6)
-  (= (levenshtein "Clojure" "Clojure") (levenshtein "" "") (levenshtein [] []) 0)
-  (= (levenshtein [1 2 3 4] [0 2 3 4 5]) 2)
-  (= (levenshtein '(:a :b :c :d) '(:a :d)) 2)
-  (= (levenshtein "ttttattttctg" "tcaaccctaccat") 10)
-  (= (levenshtein "gaattctaatctc" "caaacaaaaaattt") 9)
+  (time (= (levenshtein "kitten" "sitting") 3))
+  (time (= (levenshtein "closure" "clojure") (levenshtein "clojure" "closure") 1))
+  (time (= (levenshtein "xyx" "xyyyx") 2))
+  (time (= (levenshtein "" "123456") 6))
+  (time (= (levenshtein "Clojure" "Clojure") (levenshtein "" "") (levenshtein [] []) 0))
+  (time (= (levenshtein [1 2 3 4] [0 2 3 4 5]) 2))
+  (time (= (levenshtein '(:a :b :c :d) '(:a :d)) 2))
+  (time (= (levenshtein "ttttattttctg" "tcaaccctaccat") 10))
+  (time (= (levenshtein "gaattctaatctc" "caaacaaaaaattt") 9))
 
   ;; Some of the solutions on the web
 
-)
+  )
