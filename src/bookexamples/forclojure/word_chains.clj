675dcd3bc50c9ca12c003b9195632ddf82b976ff
Alin Posorovaschi <alinposho@yahoo.com>
2014-11-22 22:39:54 +0000

    Word chains solution that works but cannot be submitted
     - It uses def for memoization.

--- src/bookexamples/forclojure/word_chains.clj
+++ src/bookexamples/forclojure/word_chains.clj
@@ -7,15 +7,37 @@
 ;;
 ;;Write a function which takes a sequence of words, and returns true if they can be arranged into one continous word chain, and false if they cannot.
 
-(defn word-chains [words]
-  nil)
+(def levenshtein
+  (memoize
+    (fn [xs ys]
+      (cond (zero? (count xs)) (count ys)
+            (zero? (count ys)) (count xs)
+            :else (min
+                    (inc (levenshtein (rest xs) ys))
+                    (inc (levenshtein xs (rest ys)))
+                    (+ (levenshtein (rest xs) (rest ys))
+                       (if (= (first xs) (first ys)) 0 1)))))))
+
+(def word-chains?
+  (memoize
+    (fn
+      ([words] (true? (some (fn [word] (word-chains? (disj words word) [word])) words)))
+      ([words chain]
+       (if (empty? words)
+         (every? #(= 1 (apply levenshtein %)) (partition 2 1 chain))
+         (some (fn [word] (word-chains? (disj words word) (conj chain word))) words))))))
 
 
 (comment
 
 
   ;; Some of the solutions on the web
-
+  (= true (word-chains? #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"}))
+  (= false (word-chains? #{"cot" "hot" "bat" "fat"}))
+  (= false (word-chains? #{"to" "top" "stop" "tops" "toss"}))
+  (= true (word-chains? #{"spout" "do" "pot" "pout" "spot" "dot"}))
+  (= true (word-chains? #{"share" "hares" "shares" "hare" "are"}))
+  (= false (word-chains? #{"share" "hares" "hare" "are"}))
 
   )
 
