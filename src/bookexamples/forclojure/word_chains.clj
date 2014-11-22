5cc02279819cb88ecbfa6f0a3201d2329d1a00d2
Alin Posorovaschi <alinposho@yahoo.com>
2014-11-22 22:58:23 +0000

    Improved word chains solution that passes 4clojure tests

--- src/bookexamples/forclojure/word_chains.clj
+++ src/bookexamples/forclojure/word_chains.clj
@@ -27,6 +27,34 @@
          (every? #(= 1 (apply levenshtein %)) (partition 2 1 chain))
          (some (fn [word] (word-chains? (disj words word) (conj chain word))) words))))))
 
+(letfn [(dis [dis-memoized x1 x2]
+             (cond
+               (= 0 (count x1)) (count x2)
+               (= 0 (count x2)) (count x1)
+               true (apply min (remove nil? [(inc (dis-memoized dis-memoized (rest x1) (rest x2)))
+                                             (inc (dis-memoized dis-memoized x1 (rest x2)))
+                                             (inc (dis-memoized dis-memoized (rest x1) x2))
+                                             (when (= (first x1) (first x2)) (dis-memoized dis-memoized (rest x1) (rest x2)))]))))]
+  (partial dis (memoize dis)))
+
+(defn word-chains? [words]
+  (letfn [(lev [lev-memoized x1 x2]
+               (cond
+                 (= 0 (count x1)) (count x2)
+                 (= 0 (count x2)) (count x1)
+                 true (apply min (remove nil? [(inc (lev-memoized lev-memoized (rest x1) (rest x2)))
+                                               (inc (lev-memoized lev-memoized x1 (rest x2)))
+                                               (inc (lev-memoized lev-memoized (rest x1) x2))
+                                               (when (= (first x1) (first x2)) (lev-memoized lev-memoized (rest x1) (rest x2)))]))))
+          (memo-lev [x1 x2] ((partial lev (memoize lev)) x1 x2))
+          (wc ([wc-memo words] (true? (wc-memo wc-memo words [])))
+              ([wc-memo words chain]
+               (if (empty? words)
+                 (every? #(= 1 (apply memo-lev %)) (partition 2 1 chain))
+                 (some (fn [word] (wc-memo wc-memo (disj words word) (conj chain word))) words))))
+          (memo-wc [words] ((partial wc (memoize wc)) words))]
+    (memo-wc words)))
+
 
 (comment
 
