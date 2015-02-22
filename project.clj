f8b283c4ece78a545865eab205e80c9becfed08c
Alin Posorovaschi <alinposho@yahoo.com>
2015-02-22 12:20:53 +0000

    Add core.logic dependency to the project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject bookexamples "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure sample code and solution to exercises from different books. 
+  Also sample code from differen blog posts on the web."
+  :url "http://nourl.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"] 
@@ -8,7 +9,8 @@
                  [org.clojure/tools.logging "0.2.3"]
                  [log4j "1.2.16"]
                  [javax.mail/mail "1.4.1"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+                 [org.clojure/core.logic "0.8.8"]]
   ; :main ^:skip-aot bookexamples.programmingclojure.chapter9.real.world.example.pinger-main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
