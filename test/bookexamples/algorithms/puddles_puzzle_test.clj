a75a9c220c83af481bb514447e762222331d6b63
Alin Posorovaschi <alin.posorovaschi@workday.com>
2015-05-13 12:19:11 +0300

    Augments tests for the puddle puzzle solution

--- test/bookexamples/algorithms/puddles_puzzle_test.clj
+++ test/bookexamples/algorithms/puddles_puzzle_test.clj
@@ -4,9 +4,12 @@
 
 (deftest puddle-puzzle-test
   (testing "The two pass solution to the puddle puzzle problem."
-    (is (= 10 (volume [2 5 1 2 3 4 7 7 6])))
     (is (= 0 (volume [1 2 3 4 7 7 6])))
     (is (= 0 (volume nil)))
     (is (= 0 (volume [1])))
     (is (= 0 (volume [1 2])))
+    (is (= 10 (volume [2 5 1 2 3 4 7 7 6])))
+    (is (= 20 (volume [2 7 1 2 5 3 4 7 7 6])))
+    (is (= 20 (volume [2 8 1 2 5 3 4 7 7 6])))
+    (is (= 5 (volume [2 4 3 7 7 2 6])))
     ))
