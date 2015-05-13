7bb87dcc52637670176e464c6e8a5917c5544fb9
Alin Posorovaschi <alin.posorovaschi@workday.com>
2015-05-13 21:55:04 +0300

    Tests for the puddles puzzle solution with one pass

--- src/bookexamples/algorithms/puddles_puzzle.clj
+++ src/bookexamples/algorithms/puddles_puzzle.clj
@@ -5,6 +5,8 @@
 ;; can be accumulated between the walls. For example, for [2,5,1,2,3,4,7,7,6]
 ;; the amount of accumulated water is 10.
 
+;; The following solution, although functional in nature, not quite as efficient as it could be
+;; since it passes through the collections three times. The goal is to traverse the sequence only once
 (defn volume [xs]
   (if-not (seq? (seq xs))
     0
@@ -12,7 +14,14 @@
             (map (fn [lm xs rm] (- (min lm rm) xs))
                  (reductions max xs)
                  xs
-                 (reverse (reductions max (reverse xs)))))))
+                 (reverse (reductions max (reverse xs)))))))  ;; we could improve this by defining a one pass reductions right
+
+;; Solution that traverses the collection only once.
+(defn vol [xs]
+  (if-not (seq? (seq xs))
+    0
+    ))
+
 
 (comment
   (def xs [2 5 1 2 3 4 7 7 6])
--- test/bookexamples/algorithms/puddles_puzzle_test.clj
+++ test/bookexamples/algorithms/puddles_puzzle_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [bookexamples.algorithms.puddles-puzzle :refer :all]))
 
-(deftest puddle-puzzle-test
+(deftest volume-puddle-puzzle-solution-test
   (testing "The two pass solution to the puddle puzzle problem."
     (is (= 0 (volume [1 2 3 4 7 7 6])))
     (is (= 0 (volume nil)))
@@ -12,4 +12,17 @@
     (is (= 20 (volume [2 7 1 2 5 3 4 7 7 6])))
     (is (= 20 (volume [2 8 1 2 5 3 4 7 7 6])))
     (is (= 5 (volume [2 4 3 7 7 2 6])))
+    ))
+
+
+(deftest vol-puddle-puzzle-solution-test
+  (testing "The one pass solution to the puddle puzzle problem."
+    (is (= 0 (vol [1 2 3 4 7 7 6])))
+    (is (= 0 (vol nil)))
+    (is (= 0 (vol [1])))
+    (is (= 0 (vol [1 2])))
+    (is (= 10 (vol [2 5 1 2 3 4 7 7 6])))
+    (is (= 20 (vol [2 7 1 2 5 3 4 7 7 6])))
+    (is (= 20 (vol [2 8 1 2 5 3 4 7 7 6])))
+    (is (= 5 (vol [2 4 3 7 7 2 6])))
     ))
