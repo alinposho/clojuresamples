b181d53bc3bab141b493899dcd15d0c7223f25fd
Alin Posorovaschi <alin.posorovaschi@workday.com>
2015-05-20 11:18:09 +0100

    Put the code that searches Maven Central in functions

--- src/bookexamples/util/jar_searching.clj
+++ src/bookexamples/util/jar_searching.clj
@@ -4,7 +4,9 @@
             [cemerick.url :refer [url url-encode]]))
 
 
-(defn search-by-sha1 [jar-sha1]
+(defn search-by-sha1
+  "Searches maven central for available jars based on the SHA1 checksum of the jar file"
+  [jar-sha1]
   (let [maven-req-url (-> (url "http://search.maven.org/solrsearch/select")
                           (assoc :query (str "q=1:"
                                              "\""
@@ -12,27 +14,18 @@
                                              "\""
                                              "&rows=20&wt=json"))
                           str)]
-    (get-in
-      (->> (client/get maven-req-url {:throw-entire-message? true})
-           :body
-           (json/read-str))
-      ["response" "numFound"])))
+    (->> (client/get maven-req-url {:throw-entire-message? true})
+         :body
+         (json/read-str))))
+
+(defn count-found-jars [maven-central-response-map]
+  "Extaracts the number of jars found on maven central based on jar SHA1 checksum"
+  (get-in maven-central-response-map ["response" "numFound"]))
+
 
 (comment
-  (-> (url "http://search.maven.org")
-      (assoc :path (str "/#search" (url-encode (str "|ga|1|1:\"fc33bf7cd8c5309dd7b81228e8626515ee42efd9\""))))
-      str)
-  (let [maven-req-url (-> (url "http://search.maven.org/solrsearch/select")
-                          (assoc :query (str "q=1:"
-                                             (url-encode "\"fc33bf7cd8c5309dd7b81228e8626515ee42efd\"")
-                                             "&rows=20&wt=json"))
-                          str)]
-    (get-in
-      (->> (client/get maven-req-url {:throw-entire-message? true})
-           :body
-           (json/read-str))
-      ["response" "numFound"]))
 
-  (get-in {"responseHeader" {"status" 0, "QTime" 1, "params" {"fl" "id,g,a,v,p,ec,timestamp,tags", "sort" "score desc,timestamp desc,g asc,a asc,v desc", "indent" "off", "q" "1:\"fc33bf7cd8c5309dd7b81228e8626515ee42efd\"", "wt" "json", "rows" "20", "version" "2.2"}}, "response" {"numFound" 0, "start" 0, "docs" []}}
-          ["response" "numFound"])
+  (search-by-sha1 "fc33bf7cd8c5309dd7b81228e8626515ee42efd9")
+  ((comp count-found-jars search-by-sha1) "fc33bf7cd8c5309dd7b81228e8626515ee42efd9")
+
   )
